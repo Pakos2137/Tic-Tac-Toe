30f1d4949dc699de625bf397ebfec4a745ac2302
Pakos2137 <michal.pakos@Onet.pl>
2023-03-09 23:15:37 +0100

    TTT

--- kodilla-Tic-Tac-Toe/src/main/java/org/example/WinLoseDraw_Check.java
+++ kodilla-Tic-Tac-Toe/src/main/java/org/example/WinLoseDraw_Check.java
@@ -6,35 +6,31 @@ public class WinLoseDraw_Check {
             System.out.printf("Wygrałeś");
             game = false;
         }
-        if(board[0][0] == board[0][1] && board[0][2] == board[0][0]) {
+        if(board[1][0] == board[1][1] && board[1][2] == board[1][0]) {
             System.out.printf("Wygrałeś");
             game = false;
         }
-        if(board[0][0] == board[0][1] && board[0][2] == board[0][0]) {
+        if(board[2][0] == board[2][1] && board[2][2] == board[2][0]) {
             System.out.printf("Wygrałeś");
             game = false;
         }
-        if(board[0][0] == board[0][1] && board[0][2] == board[0][0]) {
+        if(board[0][0] == board[1][0] && board[2][0] == board[0][0]) {
             System.out.printf("Wygrałeś");
             game = false;
         }
-        if(board[0][0] == board[0][1] && board[0][2] == board[0][0]) {
+        if(board[1][0] == board[1][1] && board[1][2] == board[1][0]) {
             System.out.printf("Wygrałeś");
             game = false;
         }
-        if(board[0][0] == board[0][1] && board[0][2] == board[0][0]) {
+        if(board[2][0] == board[2][1] && board[2][2] == board[2][0]) {
             System.out.printf("Wygrałeś");
             game = false;
         }
-        if(board[0][0] == board[0][1] && board[0][2] == board[0][0]) {
+        if(board[0][0] == board[1][1] && board[2][2] == board[0][0]) {
             System.out.printf("Wygrałeś");
             game = false;
         }
-        if(board[0][0] == board[0][1] && board[0][2] == board[0][0]) {
-            System.out.printf("Wygrałeś");
-            game = false;
-        }
-        if(board[0][0] == board[0][1] && board[0][2] == board[0][0]) {
+        if(board[0][2] == board[1][1] && board[2][0] == board[0][2]) {
             System.out.printf("Wygrałeś");
             game = false;
         }
